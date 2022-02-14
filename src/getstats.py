a9432bcc11dffe7598400751e4effa9ed0434a92
Jonathan Otto <jonootto@gmail.com>
2022-02-14 20:51:54 +1300

    main

--- src/getstats.py
+++ src/getstats.py
@@ -4,20 +4,21 @@ import time
 import os
 import flexapi
 
-toETH = 1e-18
-address = os.environ['WALLET']
-refresh = int(os.environ['REFRESH'])
 
-gUnpaid = Gauge('flex_balance_unpaid', 'Unpaid Balance')
-gPaid = Gauge('flex_balance_paid', 'Paid Balance')
-gProfit = Gauge('flex_profit_ghash', 'Profiability/G hash')
-gBlocks = Gauge('flex_total_blocks', 'Total Blocks')
-gHash = Gauge('flex_miner_hashrate', 'Miner Hashrate')
-gMinerProfit = Gauge('flex_miner_profit', 'Current Profitability')
-
-start_http_server(8000)
 
 def main():
+	address = os.environ['WALLET']
+	refresh = int(os.environ['REFRESH'])
+
+	gUnpaid = Gauge('flex_balance_unpaid', 'Unpaid Balance')
+	gPaid = Gauge('flex_balance_paid', 'Paid Balance')
+	gProfit = Gauge('flex_profit_ghash', 'Profiability/G hash')
+	gBlocks = Gauge('flex_total_blocks', 'Total Blocks')
+	gHash = Gauge('flex_miner_hashrate', 'Miner Hashrate')
+	gMinerProfit = Gauge('flex_miner_profit', 'Current Profitability')
+
+	start_http_server(8000)
+
 	while True:
 		balance = flexapi.minerBalance(address)
 		if balance:
@@ -56,10 +57,10 @@ def main():
 
 		if (hash and profit):
 			minerprofit = (profit * hash / 1e9)
-			gProfit.set(minerprofit)
+			gMinerProfit.set(minerprofit)
 			print("Current Proftability: " + str(round(minerprofit,6)) + " ETH/Day")
 		else:
-			gProfit.set(0)
+			gMinerProfit.set(0)
 
 
 		time.sleep(refresh)
